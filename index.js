378eab545fa7e7710d3974c3a391e938cea8cf18
Arya W Pratama <aryawpratama@gmail.com>
2021-10-05 12:59:49 +0700

    Rename result

--- index.js
+++ index.js
@@ -27,12 +27,23 @@ const downloadFile = async ({ url, fileName }) => {
     if (!fs.existsSync(dir)) {
         fs.mkdirSync(dir);
     }
-    const response = await fetch(url)
-    if (!response.ok) {
-        throw new Error(`Failed to download ${response.statusText}`)
-    }
-    response.buffer().then((value) => {
-        fs.writeFile(`./download/${fileName}`,value,"utf-8",(err)=>console.log(`Writing to system ${fileName} Error:${err}`))
+    fetch(url).then((res) => {
+        if (!res.ok) {
+            throw new Error(`Failed to download ${res.statusText}`)
+        }
+        res.buffer().then((value) => {
+            console.log(`Writing to system ${fileName}`)
+            fs.writeFile(`./download/${fileName}`, value, "utf-8", (err) => {
+                if (err) {
+                    console.log(`Writing Error | Error:${err}`);
+                } else {
+                    console.log(`Writing Success`);
+                    
+                }
+            })
+        }).catch((error) => {
+            console.log(error);
+        })        
     })
 }
 
@@ -40,19 +51,18 @@ const loopDownload = () => {
     for (let i = 1; i <= loopValue; i++) {
         if (i < 10) {
             const url = `https://bestdori.com/assets/jp/movie/mv/music_video_${codeName}_hq_rip/rgmv_hq_${codeName}-00${i}.usm`
-            const fileName = `rgmv_hq_${codeName}-00${i}.usm`
+            const fileName = `rgmv_hq_${codeName}.00${i}`
             downloadFile({ url, fileName })
             
         } else if (i < 100) {
             const url = `https://bestdori.com/assets/jp/movie/mv/music_video_${codeName}_hq_rip/rgmv_hq_${codeName}-0${i}.usm`
-            const fileName = `rgmv_hq_${codeName}-0${i}.usm`
+            const fileName = `rgmv_hq_${codeName}.0${i}`
             downloadFile({ url, fileName })
         } else {
             const url = `https://bestdori.com/assets/jp/movie/mv/music_video_${codeName}_hq_rip/rgmv_hq_${codeName}-${i}.usm`
-            const fileName = `rgmv_hq_${codeName}-${i}.usm`
+            const fileName = `rgmv_hq_${codeName}.${i}`
             downloadFile({ url, fileName })
         }
     }
 
-    readLine.close()
 }
